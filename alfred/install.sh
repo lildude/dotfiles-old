a6f5c60763b04c2b60d3e5acf921ee695023f280
Colin Seymour <lildood@gmail.com>
2016-01-17 11:45:47 +0000

    Set Alfred prefs in his own install.sh

---
+++ alfred/install.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#
+# Set Alfred settings here as this needs to run after Alfred has been installed
+# with Homebrew.
+defaults write com.runningwithcrayons.Alfred-Preferences syncfolder -string "~/Dropbox/Alfred"
