c9ddbfdfe6e518aea9b9620d3ee97d517cd8653e
Colin Seymour <lildood@gmail.com>
2018-03-26 10:47:24 +0100

    Go back to simple method of installing pkgs

--- atom.symlink/install.sh
+++ atom.symlink/install.sh
@@ -10,22 +10,4 @@ fi
 
 info "Installing Atom packages"
 # Install all the pkgs in package-list.txt
-#apm-beta install --compatible --packages-file ~/.atom/package-list.txt && success "Installed Atom packages" || fail "Install Atom packages"
-
-failures=
-# apm will install everything, regardless of if it's installed or not so we need to do it manually
-# https://discuss.atom.io/t/apm-install-cmd-only-install-if-not-installed/18842
-for pkg in $(cat $HOME/.atom/package-list.txt); do
-  if [ -d "$HOME/.atom/packages/$pkg" ]; then
-    info "$pkg already installed"
-  else
-    echo "Installing $pkg" | indent
-    apm-beta install --compatible "$pkg" | indent || failures=1
-  fi
-done
-
-if [ -n "$failures" ]; then
-  fail "Some Atom packages failed to install"
-else
-  success "All Atom packages installed successfully"
-fi
+apm-beta install --compatible --packages-file ~/.atom/package-list.txt && success "Installed Atom packages" || fail "Install Atom packages"
