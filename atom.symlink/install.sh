bd2c1960a986561feedddee0388a8e6fb7d78179
Colin Seymour <lildood@gmail.com>
2018-09-03 15:13:17 +0100

    Improve atom install

--- atom.symlink/install.sh
+++ atom.symlink/install.sh
@@ -4,10 +4,14 @@ export ZSH=$HOME/.dotfiles
 source $ZSH/bin/lib.sh
 
 # Open Atom and install apm
-if ! which apm-beta > /dev/null 2>&1; then
+if ! command -v apm-beta > /dev/null 2>&1; then
   fail "Atom shell commands not installed."
 fi
 
 info "Installing Atom packages"
 # Install all the pkgs in package-list.txt
-apm-beta install --compatible --packages-file ~/.atom/package-list.txt && success "Installed Atom packages" || fail "Install Atom packages"
+if apm-beta install --compatible --packages-file ~/.atom/package-list.txt; then
+  success "Installed Atom packages"
+else
+  fail "Install Atom packages"
+fi
