c045f65b47301d6e9518a9738f61bf6e954b6b3e
Colin Seymour <lildood@gmail.com>
2018-09-03 15:14:35 +0100

    Add vscode config, pkg list and install script

---
+++ vscode/install.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+#
+export ZSH=$HOME/.dotfiles
+source $ZSH/bin/lib.sh
+
+if ! command -v code > /dev/null 2>&1; then
+  fail "VS Code shell commands not installed."
+fi
+
+info "Installing VSCode settings"
+overwrite_all=false backup_all=false skip_all=false link_file "$ZSH/vscode/settings.json" "$HOME/Library/Application Support/Code/User/settings.json"
+
+info "Installing VSCode packages"
+# Install all the pkgs in package-list.txt
+for pkg in $(cat "$ZSH/vscode/package-list.txt"); do
+  if code --install-extension $pkg > /dev/null; then
+    success "Installed $pkg package"
+  else
+    fail "Failed to install $pkg"
+  fi
+done
