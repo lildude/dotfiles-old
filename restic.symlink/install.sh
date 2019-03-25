6c7f662c3860b16f370b6c8b2f027703d7564d0b
Colin Seymour <lildood@gmail.com>
2019-03-25 10:30:58 +0000

    Fix install

--- restic.symlink/install.sh
+++ restic.symlink/install.sh
@@ -2,13 +2,14 @@
 #
 # restic
 #
+set -e
 export ZSH=$HOME/.dotfiles
 # shellcheck source=bin/lib.sh
 source "$ZSH/bin/lib.sh"
 
 if [ "$(uname -s)" = "Darwin" ]; then
   info "Installing launchd job"
-  cp "$ZSH/restic//local.restic_backup.plist" "$HOME/Library/LaunchAgents/local.restic_backup.plist"
+  cp "$ZSH/restic.symlink/local.restic_backup.plist" "$HOME/Library/LaunchAgents/local.restic_backup.plist"
   launchctl load "$HOME/Library/LaunchAgents/local.restic_backup.plist"
   if [ -f "$HOME/.restic/restic.env" ]; then
     info "Performing initial backup..."
