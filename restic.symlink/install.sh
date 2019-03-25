3e6e4cf423cb6c1a913714724e28e00d484736a2
Colin Seymour <lildood@gmail.com>
2019-03-25 16:59:02 +0000

    Unload before reloading

--- restic.symlink/install.sh
+++ restic.symlink/install.sh
@@ -9,8 +9,13 @@ source "$ZSH/bin/lib.sh"
 
 if [ "$(uname -s)" = "Darwin" ]; then
   info "Installing launchd job"
+  if launchctl list local.restic_backup > /dev/null 2>&1; then
+    launchctl unload "$HOME/Library/LaunchAgents/local.restic_backup.plist"
+  fi
   cp "$ZSH/restic.symlink/local.restic_backup.plist" "$HOME/Library/LaunchAgents/local.restic_backup.plist"
+
   launchctl load "$HOME/Library/LaunchAgents/local.restic_backup.plist"
+
   if [ -f "$HOME/.restic/restic.env" ]; then
     info "Performing initial backup..."
     launchctl start local.restic_backup
