f12377e5518b76dc2e5b0eeb5e1c0f40f25c03d8
Colin Seymour <lildood@gmail.com>
2019-08-02 14:04:48 +0100

    Shellcheck scripts

---
+++ script/shellcheck.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+#
+# Shellcheck all my bash scripts in this repo
+export ZSH=$HOME/.dotfiles
+# shellcheck source=bin/lib.sh
+source "$ZSH/bin/lib.sh"
+
+if ! command -v shellcheck > /dev/null 2>&1; then
+  fail "Shellcheck not installed."
+fi
+
+cd "$ZSH" || exit 1
+files=()
+while IFS= read -r file; do
+    # Skip travis.sh as this isn't my file.
+    [[ "$file" =~ travis\\.sh ]] && continue
+    if file --brief --mime "$file" | grep -q "x-shellscript"; then
+      files+=( "$file" )
+    fi
+done < <( git ls-files )
+
+shellcheck -x "${files[@]}"
