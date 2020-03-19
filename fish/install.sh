8999b38af2aa76bac7a2ed98c5a5fddb596435fc
Colin Seymour <lildood@gmail.com>
2020-03-19 13:12:37 +0000

    Add init fish config

--- fish/install.sh
+++ fish/install.sh
@@ -3,18 +3,30 @@
 # Exit early if Fish isn't my default shell anymore.
 [ "$DEFAULT_SHELL" != "fish" ] && exit 0
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 if [ -n "$GITHUB_WORKSPACE" ]; then
-  echo "Would attempt to change default shell to Fish" | indent
+  echo "🐠 Would attempt to change default shell to Fish" | indent
   exit 0
 fi
 
-info "Changing default shell to fish"
+if [ ! -d ~/.config/fish ]; then
+  if [ ! -d ~/.config ]; then
+    mkdir ~/.config
+  fi
+
+  ln -s "$DIR" ~/.config/fish
+  fish -c fisher
+fi
+
+info "🐠 Changing default shell to fish"
 if [ "$(uname -s)" = "Darwin" ]; then
-  if [ "$(dscl . -read "/Users/$USER" UserShell)" != "UserShell: /usr/local/bin/fish" ]; then
-    if ! grep -q /usr/local/bin/fish /etc/shells; then
-      echo '/usr/local/bin/fish' | sudo tee -a /etc/shells
+  brew_prefix=$(brew --prefix)
+  if [ "$(dscl . -read "/Users/$USER" UserShell)" != "UserShell: $brew_prefix/bin/fish" ]; then
+    if ! grep -q "$brew_prefix/bin/fish" /etc/shells; then
+      echo "$brew_prefix/bin/fish" | sudo tee -a /etc/shells
     fi
-    chsh -s /usr/local/bin/fish
+    chsh -s "$brew_prefix/bin/fish"
   fi
 else
   sudo chsh -s /bin/fish "$USER"
