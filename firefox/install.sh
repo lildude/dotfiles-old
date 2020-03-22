19b0798024080e36e4f2213e039a721606fdce99
Colin Seymour <lildood@gmail.com>
2020-03-22 10:55:46 +0000

    Add firefox config

---
+++ firefox/install.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+#
+# Disable for now.
+exit 0
+
+export ZSH=$HOME/.dotfiles
+# shellcheck source=bin/lib.sh
+source "$ZSH/bin/lib.sh"
+
+# Determine Firefox profile path - assumes we've kept the `.default` at the end of the path
+if [ -d "$HOME/Library/Application Support/Firefox/Profiles/" ]; then
+  PROFILE_DIR=$(find "$HOME/Library/Application Support/Firefox/Profiles/" -type d -depth 1 -name "*.default" | head -1)
+else
+  info "No default profile found. Starting Firefox to create one"
+  /Applications/Firefox.app/Contents/MacOS/firefox-bin --headless
+  exec "$0"
+fi
+info "Installing Firefox ghacks user.js and your overrides"
+"$ZSH/firefox/updater.sh" -P "$PROFILE_DIR" -u -s
