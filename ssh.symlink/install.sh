f6d97a0545eaa6d2e50eba3c1b7a5707ac803eb7
Colin Seymour <lildood@gmail.com>
2020-03-29 14:09:16 +0100

    Add SSH config that pulls keys from 1Password

---
+++ ssh.symlink/install.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+#
+set -e
+export DOTFILES_ROOT=$HOME/.dotfiles
+# shellcheck source=bin/lib.sh
+source "$DOTFILES_ROOT/bin/lib.sh"
+
+# Login if we're not already
+# shellcheck disable=SC2154 # the eval below sets it
+if [ -z "$OP_SESSION_my" ]; then
+  eval "$(op signin my)"
+fi
+# Grab all my SSH keys from 1Password using the CLI
+declare -A keys
+while IFS="=" read -r key value; do
+  keys["$key"]="$value"
+done < <(op list documents | jq -r '.[] | select(.overview.title | contains("SSH Key") ) | { (.uuid): .overview.title } | to_entries | .[] | .key + "=" + .value')
+#declare -p keys
+
+#cd "$HOME/.ssh/" || exit 1
+
+for i in "${!keys[@]}"; do
+  filename=$(echo "${keys[$i]}" | awk '{ printf $NF}')
+  if [ ! -f "$filename" ]; then
+    info "Saving ${keys[$i]} from 1Password"
+    op get document "$i" > "$filename"
+    chmod 600 "$filename"
+  fi
+done
+
+#cd "$HOME" || exit
