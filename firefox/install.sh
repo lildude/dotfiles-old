de4e57db0370a7822a9c883ab82514a2700f7b9d
Colin Seymour <lildood@gmail.com>
2020-04-12 14:05:04 +0100

    Refactoring

--- firefox/install.sh
+++ firefox/install.sh
@@ -4,20 +4,21 @@ DOTFILES=$(cd "$(dirname "$0")/.." && pwd)
 # shellcheck source=bin/lib.sh
 source "$DOTFILES/bin/lib.sh"
 
-# Comment me out when finished testing
-#PROFILE_DIR="$HOME/Library/Application Support/Firefox/Profiles/897x8zng.gHacksTest"
-
-# Determine Firefox profile path - assumes we've kept the `.default` at the end of the path
-if [ -z "$PROFILE_DIR" ]; then
-  if [ -d "$HOME/Library/Application Support/Firefox/Profiles/" ]; then
-    PROFILE_DIR=$(find "$HOME/Library/Application Support/Firefox/Profiles/" -type d -depth 1 -name "*.default" | head -1)
-  else
-    info "No default profile found. Starting Firefox to create one"
-    /Applications/Firefox.app/Contents/MacOS/firefox-bin --headless
-    exec "$0"
-  fi
+if [ $MACOS ]; then
+  PROFILES_DIR="$HOME/Library/Application Support/Firefox/Profiles/"
+elif [ $LINUX ]; then
+  PROFILES_DIR="$HOME/.mozilla/firefox/"
 fi
-info "Installing Firefox ghacks user.js and my overrides to $PROFILE_DIR"
-ln -s "$DOTFILES/firefox/user-overrides.js" "$PROFILE_DIR/user-overrides.js"
-ln -s "$DOTFILES/firefox/updater.sh" "$PROFILE_DIR/updater.sh"
-"$DOTFILES/firefox/updater.sh" -p "$PROFILE_DIR" -u -s
+
+PROFILE="lildude.default"
+#PROFILE="897x8zng.gHacksTest"  # Debugging ghacks profile
+
+PROFILE_PATH="$PROFILES_DIR/$PROFILE"
+if [ ! -d "$PROFILE_PATH" ]; then
+  mkdir -p "$PROFILE_PATH"
+fi
+
+info "Installing Firefox ghacks user.js and my overrides to $PROFILE_PATH"
+link_file "$DOTFILES/firefox/user-overrides.js" "$PROFILE_PATH/user-overrides.js"
+link_file "$DOTFILES/firefox/updater.sh" "$PROFILE_PATH/updater.sh"
+"$DOTFILES/firefox/updater.sh" -p "$PROFILE_PATH" -u -s
--- homebrew/install.sh
+++ homebrew/install.sh
@@ -8,17 +8,43 @@ DOTFILES=$(cd "$(dirname "$0")/.." && pwd)
 # shellcheck source=bin/lib.sh
 source "$DOTFILES/bin/lib.sh"
 
+# I only use homebrew on the mac at the mo
 if [ $MACOS ]; then
   # Check for Homebrew
   if ! command -v brew > /dev/null 2>&1; then
-    info "Installing Homebrew for you."
+    echo "Installing Homebrew"
+    bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+  fi
 
-    # Install the correct homebrew for each OS type
-    if [ $MACOS ]; then
-      ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-    elif [ $LINUX ]; then
-      ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/linuxbrew/go/install)"
+  # Update homebrew
+  brew update > /dev/null 2>&1
+
+  # Ensure everything in the Brewfile is installed
+  brew_check=$(brew bundle check --verbose 2>&1)
+  if [ -n "$brew_check" ]; then
+    if [ -n "$GITHUB_WORKSPACE" ]; then
+      echo "$brew_check" | indent
+    else
+      brew bundle install --file "$DOTFILES/Brewfile" | indent
+    fi
+  fi
+
+  # Upgrade homebrew pkgs, but not in CI as we don't care about most of the pre-installed pkgs
+  if [ -z "$GITHUB_WORKSPACE" ]; then
+    if [ "$(brew outdated | wc -l)" -gt 0 ]; then
+      if brew upgrade > /dev/null; then success "Upgraded brew packages"; else fail "Upgrade brew packages"; fi
     fi
 
+    if [ "$(brew cask outdated | wc -l)" -gt 0 ]; then
+      if brew cask upgrade > /dev/null; then success "Upgraded brew cask packages"; else fail "Upgrade brew cask packages"; fi
+    fi
+
+    # Clean up
+    if [ "$(brew cleanup -n 2> /dev/null | wc -l)" -gt 0 ]; then
+      if brew cleanup > /dev/null 2>&1; then success "Brew cleaned up"; else fail "Brew clean up"; fi
+    fi
   fi
+
+else
+  success "Skipped installing homebrew"
 fi
