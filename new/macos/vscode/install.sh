4b982abc123d35ef263c21f54bfdfac11870fb55
Colin Seymour <lildood@gmail.com>
2020-07-23 11:56:27 +0100

    Add vscode

---
+++ new/macos/vscode/install.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+#
+set -euo pipefail
+
+DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)
+# shellcheck source=script/lib.sh
+source "$DIR/../script/lib.sh"
+
+# Exit early on Linux for the mo
+[ $LINUX ] && exit 0
+
+if ! command -v code > /dev/null 2>&1; then
+  fail "VS Code shell commands not installed."
+fi
+
+info "Installing VSCode settings"
+ln -Ff -s "$DIR/vscode/settings.json" "$HOME/Library/Application Support/Code/User/settings.json"
+ln -Ff -s "$DIR/vscode/keybindings.json" "$HOME/Library/Application Support/Code/User/keybindings.json"
+
+EXTENSIONS="$(code --list-extensions)"
+
+info "Installing VSCode packages"
+# Install all the pkgs in package-list.txt
+# Update this using: code --list-extensions >! $HOME/.dotfiles/vscode/package-list.txt
+while IFS= read -r ext; do
+  if echo "$EXTENSIONS" | grep -q "$ext"; then
+    info "Extension $ext already installed."
+  else
+    if code --install-extension "$ext" > /dev/null; then
+      info "Installed $ext package"
+    else
+      fail "Failed to install $ext"
+    fi
+  fi
+done < <(grep -v '^ *#' < "$DIR/vscode/package-list.txt")
