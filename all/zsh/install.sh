6306410be38f8307c0244f646cfd0355ea5e30a0
Colin Seymour <lildood@gmail.com>
2020-08-23 12:58:04 +0100

    Shellcheck

--- all/zsh/install.sh
+++ all/zsh/install.sh
@@ -9,30 +9,27 @@ source "$DIR/../script/lib.sh"
 # Exit early if ZSH isn't my default shell
 [ "$DEFAULT_SHELL" != "zsh" ] && exit 0
 
-if [ ! -d ${HOME}/.zprezto ]; then
+if [ ! -d "${HOME}/.zprezto" ]; then
   info "Downloading prezto"
   git clone -q --recursive https://github.com/lildude/prezto.git "$HOME/.zprezto"
 else
   info "Updating .zpreto from fork on GitHub (previously updated using 'zprezto-update')"
-  cd ${HOME}/.zprezto
+  cd "${HOME}/.zprezto"
   git pull -q
-  cd ${HOME}
+  cd "${HOME}"
 fi
 
 info "Installing ZSH rc files"
 setopt EXTENDED_GLOB
-for rcfile in "$HOME"/.zprezto/runcoms/^(README.md|zlogout)(.N); do
-  if [ ! -L "$HOME/.${rcfile:t}" ]; then
-    if [ -f "$HOME/.${rcfile:t}" ]; then
-      echo "Backing up $HOME/.${rcfile:t} to $HOME/.${rcfile:t}.bak"
-      mv $HOME/.${rcfile:t} $HOME/.${rcfile:t}.bak
-    fi
-    overwrite_all=false backup_all=false skip_all=false link_file "$rcfile" "$HOME/.${rcfile:t}"
+while IFS= read -r -d '' src; do
+  [ "$src" == "README" ] || [ "$src" == "zlogout" ] && continue
+  if [ ! -L "$HOME/.${src:t}" ]; then
+    ln -Ffs "$src" "$HOME/.${src:t}"
   fi
 done
 
-if [ "$(uname -s)" = "Darwin" ]; then
-  if [ "$(dscl . -read /Users/$USER UserShell)" != "UserShell: /usr/local/bin/zsh" ]; then
+if [ "$MACOS" ]; then
+  if [ "$(dscl . -read "/Users/$USER" UserShell)" != "UserShell: /usr/local/bin/zsh" ]; then
     info "Changing default shell to zsh"
     if ! grep -q /usr/local/bin/zsh /etc/shells; then
       echo '/usr/local/bin/zsh' | sudo tee -a /etc/shells
@@ -40,5 +37,5 @@ if [ "$(uname -s)" = "Darwin" ]; then
     chsh -s /usr/local/bin/zsh
   fi
 else
-  sudo chsh -s /bin/zsh $USER
+  sudo chsh -s /bin/zsh "$USER"
 fi
