8999baa0734d6485b90f513f96604d1a5bc85bd2
Colin Seymour <lildood@gmail.com>
2020-09-28 10:48:48 +0100

    Fix syntax issues

--- all/fish/config.fish
+++ all/fish/config.fish
@@ -89,8 +89,8 @@ set -U fish_pager_color_prefix      'white' '--bold' '--underline'
 set -U fish_pager_color_progress    'brwhite' '--background=cyan'
 
 # Change this to set -x PATH
-[ "$(uname -s)" = "Darwin" ] && OS=macos
-[ "$(uname -s)" = "Linux" ] && OS=linux
+[ "(uname -s)" = "Darwin" ] && set OS macos
+[ "(uname -s)" = "Linux" ] && set OS linux
 
 set -g fish_user_paths "$HOME/.dotfiles/all/bin" $fish_user_paths
 set -g fish_user_paths "$HOME/.dotfiles/$OS/bin" $fish_user_paths
