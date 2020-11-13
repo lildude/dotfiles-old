79f8fce8c6e6478b7bdba48ce5da0b6554587b59
Colin Seymour <colin@symr.io>
2020-11-13 15:06:02 +0000

    Fix unbound var

--- macos/install.sh
+++ macos/install.sh
@@ -11,7 +11,7 @@ DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)
 source "$DIR/script/lib.sh"
 
 # Don't run on Linux or under GitHub Actions (aka CI)
-if [ $MACOS ] && [ -z "$CI" ]; then
+if [ $MACOS ] && [ -z "${CI:-}" ]; then
   info "sudo softwareupdate -i -a"
   sudo softwareupdate -i -a 2>&1
   "$DIR/set-defaults.sh"
