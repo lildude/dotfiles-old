fe8a63d54c5b9819644cc4b83fa9a6ba55e955ab
Colin Seymour <colin@symr.io>
2021-03-21 13:08:42 +0000

    Essentially noop for now

--- linux/install.sh
+++ linux/install.sh
@@ -6,9 +6,3 @@ set -euo pipefail
 DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)
 # shellcheck source=script/lib.sh
 source "$DIR/script/lib.sh"
-
-# Don't run on macOS or under GitHub Actions (aka CI)
-if [ $LINUX ] && [ -z "${CI:-}" ]; then
-  info "   … Tweaking .gitconfig"
-  git config --global --remove-section credential
-fi
